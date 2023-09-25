d1ca1f57dfcaa2ca7ce0507d09f57c00ae1c75be
Ivanescarraga103 <elproivan002@gmail.com>
2023-09-24 20:39:53 -0500

    mi primer commit

---
+++ Proyecto8.java
@@ -0,0 +1,7 @@
+package com.mycompany.Ivan;
+public class Proyecto8 {
+public static void main(String[] args) {
+        System.out.print("Hola mundo");
+        System.out.print("Ivan Azcarraga Mercado");
+    }
+}
